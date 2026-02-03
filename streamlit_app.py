cc76f3ffa496614d83f2943e90473290d61ffc96
UtkarsH-1610 <bulusu163346@mastek.com>
2026-02-03 09:50:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,6 @@ my_dataframe = session.table(
 
 pd_df = my_dataframe.to_pandas()
 
-# st.dataframe(pd_df)
-
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:',
     pd_df['FRUIT_NAME'].tolist(),
@@ -31,10 +29,19 @@ if ingredients_list:
     ingredients_string = ", ".join(ingredients_list)
 
     for fruit_chosen in ingredients_list:
-        ingredients_String +=fruit_chosen + ''
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen,'SEARCH_ON'].iloc[0]
-        st.write('The search value for ' , fruit_chosen,'is ', seaarch_on, '.')
+        search_on = pd_df.loc[
+            pd_df['FRUIT_NAME'] == fruit_chosen,
+            'SEARCH_ON'
+        ].iloc[0]
+
+        st.write(
+            'The search value for',
+            fruit_chosen,
+            'is',
+            search_on,
+            '.'
+        )
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
@@ -42,7 +49,10 @@ if ingredients_list:
             f"https://my.fruitvice.com/api/fruit/{search_on}"
         )
 
-        st.dataframe(response.json(), use_container_width=True)
+        st.dataframe(
+            fruitvice_response.json(),
+            use_container_width=True
+        )
 
     my_insert_stmt = f"""
         INSERT INTO smoothies.public.orders (ingredients, name_on_order)
